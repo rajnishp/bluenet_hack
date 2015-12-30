68d0cc7517c7624e8e746a71fc46dc8206815053
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2015-12-30 22:41:28 +0530

    done

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 
 	
-$db_handle = mysqli_connect("localhost","root","redhat111111","bluenethack");
+$db_handle = mysqli_connect("localhost","root","redhat@11111p","bluenethack");
 
 //Check connection
 	if (mysqli_connect_errno()) {
