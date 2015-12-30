a1358a6c5d8996872590bf323b86d52c0592b003
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2015-12-30 09:41:36 +0530

    view complete

--- cem_view.php
+++ cem_view.php
@@ -1,21 +1,23 @@
 <?php
 
-	$db_handle = mysqli_connect("localhost","root","redhat11111p","bluenethack");
+	$db_handle = mysqli_connect("localhost","root","redhat111111","bluenethack");
 
 //Check connection
 	if (mysqli_connect_errno()) {
 	  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 
-	$sr_id = $_GET["id"];
+	$status = $_GET["status"];
 
 	if (isset($_POST['update_status'])) {
-		
 		$newStatus = $_POST['new_status'];
-		
+		$sr_id = $_POST['sr_id'];
 		$sql = mysqli_query ($db_handle, "UPDATE service_request SET status= '$newStatus' WHERE id = '$sr_id' ;");
-
-
+		if(mysqli_connect_errno()){		
+		}
+		else { 
+			header("Location: cem_view.php"); 
+		}
 	}
 
 	if (isset($_POST['update_sr'])) {
@@ -24,13 +26,8 @@
 		$match1_mobile = $_POST['match1_mobile'];
 		$match2_mobile = $_POST['match2_mobile'];
 		$match2_name = $_POST['match2_name'];
-		
-		
-		$sql = mysqli_query ($db_handle, "UPDATE `service_request` SET `status`= $newStatus, 
-													`match_name`= $match1_name, 
-													`match2_name`= $match2_name, 
-													`match_mobile`=$match_mobile  WHERE `id` =  $sr_id ;");
-
+		$sql = mysqli_query ($db_handle, "UPDATE `service_request` SET `status`= $newStatus, match_name`= $match1_name, match2_name`= $match2_name, 
+													`match_mobile`=$match_mobile, `match2_mobile`=$match2_mobile  WHERE `id` =  $sr_id ;");
 	}
 
 ?>
@@ -51,23 +48,25 @@
 
 
 </head>
-
 <body>
 	<div id="container" class="effect mainnav-lg">
 		
 		<table id="example" class="display" cellspacing="0" width="100%">
 	        <thead>
 	            <tr>
+					<th></th>
 	                <th>Name</th>
 	                <th>Mobile</th>
 	                <th>Requirement</th>
+	                <th>Gender</th>
 	                <th>Timing</th>
 	                <th>Salary</th>
 	                <th>Address</th>
 	                <th>Remarks</th>
+	                <th>Date</th>
 	                <th>Match 1</th>
 	                <th>Match 2</th>
-	                <th></th>
+	                <th>Status</th>
 	            </tr>
 	        </thead>
 	        <tfoot>
@@ -77,19 +76,25 @@
 	        </tfoot>
 	        <tbody>
 	            <tr>
-	            	<?php 
-	            		
-		            	$srs = mysqli_query($db_handle, "SELECT * FROM service_request WHERE id = $sr_id; ") ;
+	            	<?php
+						if(isset($status)){
+							$srs = mysqli_query($db_handle, "SELECT * FROM service_request WHERE status = '$status'; ") ;
+						}
+						else {
+							$srs = mysqli_query($db_handle, "SELECT * FROM service_request ") ;
+						}
 						while ($srsrow = mysqli_fetch_array($srs)){
 					?>
 					
 	                <td><?= $srsrow['name'] ?> </td>
 	                <td><?= $srsrow['mobile'] ?> </td>
 	                <td><?= $srsrow['requirements'] ?> </td>
+	                <td><?= $srsrow['gender'] ?> </td>
 	                <td><?= $srsrow['timings'] ?> </td>
 	                <td><?= $srsrow['expected_salary'] ?> </td>
 	                <td><?= $srsrow['address'] ?> </td>
 	                <td><?= $srsrow['remarks'] ?> </td>
+	                <td><?= $srsrow['date'] ?> </td>
 	                <td><?= $srsrow['match_name'] ?> <?= $srsrow['match_mobile'] ?> </td>
 	                <td><?= $srsrow['match2_name'] ?> <?= $srsrow['match2_mobile'] ?> </td>
 	                <td>
@@ -103,6 +108,7 @@
 								<option value="decay" >Decay</option>
 
 							</select>
+							<input type="hidden" name="sr_id" value="<?= $srsrow['id'] ?>">
 							<button type="submit" name="update_status" class="btn btn-primary"> Update </button>
 						</form>
 	                </td>
