e3385c66094b594fe6356e311e47d5b19750ce58
Rajnish Panwar <rajnish_pawar90@yahoo.com>
2016-01-01 15:33:04 +0530

    done

--- findAreaRequests.php
+++ findAreaRequests.php
@@ -3,7 +3,7 @@
 
 $area = $_GET['area'];
 
-$mysqli = new mysqli('localhost','root','redhat111111', 'bluenethack');
+$mysqli = new mysqli('localhost','root','redhat@11111p', 'bluenethack');
 	if ($mysqli->connect_errno) {
     	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		return 1;
